d0e7b7b21e393474bfedd3206c2c0e3d5e1671bf
Trevor Scholz <trevorscholz1@icloud.com>
2025-01-04 15:40:30 -0500

    weekly

--- daily_lockz/models/bets.py
+++ daily_lockz/models/bets.py
@@ -5,9 +5,10 @@ import pandas as pd
 
 TEST = 0
 DATE = date.today() + timedelta(days=TEST)
+DATESEED = int(str(DATE).replace('-', ''))
 
-np.random.seed(int(str(DATE).replace('-', '')))
-print(int(str(DATE).replace('-', '')))
+np.random.seed(DATESEED)
+print(DATESEED)
 
 try:
     os.remove('./trevorAppsWebsites/daily-lockz/public/all_sims.csv')
@@ -27,8 +28,8 @@ soccer_sims['is_dl'] = False
 sims['datetime'] = pd.to_datetime(sims['time'], format='%m/%d/%Y %I:%M %p')
 soccer_sims['datetime'] = pd.to_datetime(soccer_sims['time'], format='%m/%d/%Y %I:%M %p')
 
-sims = sims.sort_values(by=['datetime', 'home_team']).reset_index(drop=True)
-soccer_sims = soccer_sims.sort_values(by=['datetime', 'home_team']).reset_index(drop=True)
+sims = sims.sort_values(by=['datetime','home_team']).reset_index(drop=True)
+soccer_sims = soccer_sims.sort_values(by=['datetime','home_team']).reset_index(drop=True)
 
 dl_indices = []
 for sport, group in sims.groupby('sport'):
@@ -47,12 +48,12 @@ soccer_sims.loc[dl_indices, 'is_dl'] = True
 all_sims = pd.concat([sims, soccer_sims], ignore_index=True)
 all_sims['cur_date'] = DATE
 all_sims['datetime'] = pd.to_datetime(all_sims['datetime'], format='%m/%d/%Y %I:%M %p')
-all_sims = all_sims.sort_values(by=['datetime', 'home_team']).reset_index(drop=True)
+all_sims = all_sims.sort_values(by=['datetime','home_team']).reset_index(drop=True)
 all_sims = all_sims.drop(columns=['datetime'])
 
-print(f"GAMES AVAILABLE: {len(all_sims[all_sims['is_dl'] == True])}")
 all_sims.to_csv('./trevorAppsWebsites/daily-lockz/public/all_sims.csv', index=False, header=True)
 all_sims.to_csv('./trevorAppsWebsites/DailyLockz/all_sims.csv', index=False, header=True)
+print(f"GAMES AVAILABLE: {len(all_sims[all_sims['is_dl'] == True])}")
 
 os.chdir('trevorAppsWebsites/daily-lockz')
 os.system('git add .')
@@ -67,16 +68,37 @@ bets['spread'] = all_sims['h_score'] - all_sims['a_score']
 bets['total_score'] = all_sims['h_score'] + all_sims['a_score']
 print(bets[['sport','home_team','away_team','h_score','a_score','implied_odds','spread','total_score','time']])
 
+i = 0
+assignments = {}
 bets.sort_values(by='sport', inplace=True)
 for index, row in bets.iterrows():
-    if row['sport'] == 'NBA' or row['sport'] == 'NCAAB' or row['sport'] == 'NCAAF' or row['sport'] == 'NFL':
-        if row['h_score'] >= row['a_score']:
-            print(f"{row['home_team']} by {abs(row['spread'])} points ||| {row['total_score']}")
-        else:
-            print(f"{row['away_team']} by {abs(row['spread'])} points ||| {row['total_score']}")
+    np.random.seed(i*DATESEED)
+    if row['sport'] not in assignments:
+        assignments[row['sport']] = []
+
+    if len(assignments[row['sport']]) == 0:
+        assignment = np.random.choice(['Spread','Total'])
+    elif len(assignments[row['sport']]) == 1:
+        assignment = 'Spread' if assignments[row['sport']][0] == 'Total' else 'Total'
     else:
-        if row['h_score'] >= row['a_score']:
-            print(f"{row['home_team']} at {row['implied_odds']} ||| {row['total_score']}")
+        continue
+
+    if row['h_score'] >= row['a_score']:
+        winteam = row['home_team']
+    else:
+        winteam = row['away_team']
+
+    assignments[row['sport']].append(assignment)
+    if row['sport'] in ['NBA', 'NCAAB', 'NCAAF', 'NFL']:
+        if assignment == 'Spread':
+            print(f"{row['sport']} {row['time']} {row['home_team']}/{row['away_team']}: {assignment} {winteam} by {abs(row['spread'])}")
         else:
-            print(f"{row['away_team']} at {row['implied_odds']} ||| {row['total_score']}")
+            print(f"{row['sport']} {row['time']} {row['home_team']}/{row['away_team']}: {assignment} {row['total_score']}")
+    else:
+        if assignment == 'Spread':
+            print(f"{row['sport']} {row['time']} {row['home_team']}/{row['away_team']}: {assignment} {winteam} at {row['implied_odds']}")
+        else:
+            print(f"{row['sport']} {row['time']} {row['home_team']}/{row['away_team']}: {assignment} {row['total_score']}")
+        
     print('--------------------------------------------------')
+    i += 1
