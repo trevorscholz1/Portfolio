398cad2c4c894adf3df05d089a783e8f95d12deb
Trevor Scholz <trevorscholz1@icloud.com>
2025-07-12 10:45:19 -0400

    weekly

--- daily_lockz/models/odds.py
+++ daily_lockz/models/odds.py
@@ -1,4 +1,4 @@
-from datetime import datetime as dt
+from datetime import date, datetime as dt
 from dotenv import load_dotenv
 from macnotesapp import NotesApp
 import os
@@ -31,11 +31,18 @@ def main():
     spacer = ''
     for _ in range(50):
         spacer += '|'
-    
-    BETS = pd.DataFrame(columns=['sport','team','odds','time'])
+
+    BETS = pd.DataFrame(columns=['sport','team','type','point','odds','time'])
 
     load_dotenv()
-    API_KEY = os.getenv('ODDS_API_KEY')
+    DATE = date.today()
+    DATESEED = int(str(DATE).replace('-', ''))
+    if DATESEED % 2 == 0:
+        API_KEY = os.getenv('ODDS_API_KEY')
+        BODY += 'USING MAIN'
+    else:
+        API_KEY = os.getenv('ODDS_BACKUP_KEY')
+        BODY += 'USING BACKUP'
 
     ACTIVE_SPORTS = []
     SPORTS = ['baseball_mlb','basketball_nba','basketball_ncaab','americanfootball_ncaaf','americanfootball_nfl','icehockey_nhl','soccer_usa_mls']
@@ -46,7 +53,6 @@ def main():
         sports_data = response.json()
     except Exception as e:
         print('Error fetching sports:', e)
-        return
 
     for sport in sports_data:
         if sport['key'] in SPORTS and sport['active']:
@@ -54,56 +60,97 @@ def main():
 
     for sport in ACTIVE_SPORTS:
         try:
-            response = requests.get(f"https://api.the-odds-api.com/v4/sports/{sport}/odds/?apiKey={API_KEY}&regions=us,us2&markets=h2h")
+            response = requests.get(f"https://api.the-odds-api.com/v4/sports/{sport}/odds/?apiKey={API_KEY}&regions=us,us2&markets=h2h,spreads,totals")
             response.raise_for_status()
             games = response.json()
         except Exception as e:
             print(f"Error fetching odds for {sport}:", e)
-            return
-        
+            
         BODY += (f"PROCESSED {len(games)} {sport.split('_')[-1].upper()} GAMES")
 
         for game in games:
+            index = 0
+            GAME = pd.DataFrame(columns=['sport','h_team','a_team','book','h_ml','a_ml','h_spread','a_spread','spread_point','over','under','total_point'])
+        
             home_team = game['home_team']
             away_team = game['away_team']
-            home_odds = []
-            away_odds = []
-            fliff_h_odds = 0
-            fliff_a_odds = 0
             
             time, live = convert_time(game['commence_time'])
             if live:
                 continue
 
             for book in game['bookmakers']:
-                outcomes = book['markets'][0]['outcomes']
-                if outcomes[0]['name'] == home_team:
-                    home_odd = outcomes[0]['price']
-                    away_odd = outcomes[1]['price']
-                else:
-                    home_odd = outcomes[1]['price']
-                    away_odd = outcomes[0]['price']
-
-                if book['title'] == 'Fliff':
-                    fliff_h_odds = home_odd
-                    fliff_a_odds = away_odd
-
-                home_odds.append(home_odd)
-                away_odds.append(away_odd)
+                GAME.at[index, 'sport'] = sport
+                GAME.at[index, 'h_team'] = home_team
+                GAME.at[index, 'a_team'] = away_team
+                GAME.at[index, 'book'] = book['key']
+                
+                for market in book['markets']:
+                    outcomes = market['outcomes']
+                    if outcomes[0]['name'] == home_team or outcomes[0]['name'] == 'Over':
+                        if market['key'] == 'h2h':
+                            GAME.at[index, 'h_ml'] = outcomes[0]['price']
+                            GAME.at[index, 'a_ml'] = outcomes[1]['price']
+                        elif market['key'] == 'spreads':
+                            GAME.at[index, 'h_spread'] = outcomes[0]['price']
+                            GAME.at[index, 'a_spread'] = outcomes[1]['price']
+                            GAME.at[index, 'spread_point'] = outcomes[0]['point']
+                        elif market['key'] == 'totals':
+                            GAME.at[index, 'over'] = outcomes[0]['price']
+                            GAME.at[index, 'under'] = outcomes[1]['price']
+                            GAME.at[index, 'total_point'] = outcomes[0]['point']
+                    elif outcomes[1]['name'] == home_team or outcomes[1]['name'] == 'Over':
+                        if market['key'] == 'h2h':
+                            GAME.at[index, 'h_ml'] = outcomes[1]['price']
+                            GAME.at[index, 'a_ml'] = outcomes[0]['price']
+                        elif market['key'] == 'spreads':
+                            GAME.at[index, 'h_spread'] = outcomes[1]['price']
+                            GAME.at[index, 'a_spread'] = outcomes[0]['price']
+                            GAME.at[index, 'spread_point'] = outcomes[1]['point']
+                        elif market['key'] == 'totals':
+                            GAME.at[index, 'over'] = outcomes[1]['price']
+                            GAME.at[index, 'under'] = outcomes[0]['price']
+                            GAME.at[index, 'total_point'] = outcomes[1]['point']
+                index += 1
             
-            if not len(home_odds) or not len(away_odds):
-                continue
+            fliff = GAME[GAME['book'] == 'fliff'].reset_index()
+            if not fliff['h_ml'].empty and fliff['h_ml'][0] >= (GAME['h_ml'].mean() + 0.03):
+                odds = convert(fliff['h_ml'][0])
+                BETS.loc[len(BETS)] = {'sport': sport, 'team': home_team, 'type': 'ML', 'point': 0, 'odds': odds, 'time': time}
+                
+            if not fliff['a_ml'].empty and fliff['a_ml'][0] >= (GAME['a_ml'].mean() + 0.03):
+                odds = convert(fliff['a_ml'][0])
+                BETS.loc[len(BETS)] = {'sport': sport, 'team': away_team, 'type': 'ML', 'point': 0, 'odds': odds, 'time': time}
 
-            h_avg = sum(home_odds) / len(home_odds)
-            a_avg = sum(away_odds) / len(away_odds)
+            if not fliff.empty:
+                SPREAD = GAME[GAME['spread_point'] == fliff['spread_point'][0]].reset_index()
+            else:
+                SPREAD = []
+            if len(SPREAD) > 1:
+                if not fliff['h_spread'].empty and fliff['h_spread'][0] >= (SPREAD['h_spread'].mean() + 0.03):
+                    odds = convert(fliff['h_spread'][0])
+                    point = fliff['spread_point'][0]
+                    BETS.loc[len(BETS)] = {'sport': sport, 'team': home_team, 'type': 'SPREAD', 'point': point, 'odds': odds, 'time': time}
+                    
+                if not fliff['a_spread'].empty and fliff['a_spread'][0] >= (SPREAD['a_spread'].mean() + 0.03):
+                    odds = convert(fliff['a_spread'][0])
+                    point = fliff['spread_point'][0]
+                    BETS.loc[len(BETS)] = {'sport': sport, 'team': away_team, 'type': 'SPREAD', 'point': point, 'odds': odds, 'time': time}
 
-            if fliff_h_odds and fliff_h_odds >= (h_avg+0.03):
-                odds = convert(fliff_h_odds)
-                BETS.loc[len(BETS)] = {'sport': sport, 'team': home_team, 'odds': odds, 'time': time}
-
-            if fliff_a_odds and fliff_a_odds >= (a_avg+0.03):
-                odds = convert(fliff_a_odds)
-                BETS.loc[len(BETS)] = {'sport': sport, 'team': away_team, 'odds': odds, 'time': time}
+            if not fliff.empty:
+                TOTAL = GAME[GAME['total_point'] == fliff['total_point'][0]].reset_index()
+            else:
+                TOTAL = []
+            if len(TOTAL) > 1:
+                if not fliff['over'].empty and fliff['over'][0] >= (TOTAL['over'].mean() + 0.03):
+                    odds = convert(fliff['over'][0])
+                    point = fliff['total_point'][0]
+                    BETS.loc[len(BETS)] = {'sport': sport, 'team': home_team, 'type': 'OVER', 'point': point, 'odds': odds, 'time': time}
+                    
+                if not fliff['under'].empty and fliff['under'][0] >= (TOTAL['under'].mean() + 0.03):
+                    odds = convert(fliff['under'][0])
+                    point = fliff['total_point'][0]
+                    BETS.loc[len(BETS)] = {'sport': sport, 'team': away_team, 'type': 'UNDER', 'point': point, 'odds': odds, 'time': time}
     
     BODY += '-'
     for _, row in BETS.iterrows():
